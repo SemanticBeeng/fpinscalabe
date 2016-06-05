2f0071947b73a6965bd653682538f63bc3e77c24
SemanticBeeng <nick@email.com>
2016-06-05 09:21:01 +0000

    first Functor examples

--- src/test/scala/org/specs2/examples/FunctorExamples.scala
+++ src/test/scala/org/specs2/examples/FunctorExamples.scala
@@ -9,12 +9,12 @@ import std.list._
 
 object FunctorExamples extends UserGuidePage { def is = "Functor Examples".title ^ s2"""
 
-  Applying a function to a [[Functor]] preserves the type of the [[Functor]].: ${snippet{
+  Applying a function to a [[Functor]] preserves the type/shape of the [[Functor]].: ${snippet{
 
   val len: String => Int = _.length
 
   class SeqSpecification extends mutable.Specification {
-    "updateLast modifies the last element of a Seq".p
+    "Examples for [[Functor]]s like Option and List ".p
     eg { Functor[Option].map(Some("adsf"))(len)         must_== Some(4)   }
     eg { Functor[Option].map(None)(len)                 must_== None      }
     eg { Functor[List].map(List("qwer", "adsfg"))(len)  must_== List(4,5) }
