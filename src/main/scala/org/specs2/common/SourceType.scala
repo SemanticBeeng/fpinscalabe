5aa0eaf2ca5026eb790e64fdfc3e6d9ce63972c2
SemanticBeeng <nick@email.com>
2016-06-06 11:06:44 +0000

    (checkpoint) Changed structure discipline to include object Specs as per @etorreborre advice (checkpoint) Now specs are actually evaluated/checked.

--- src/main/scala/org/specs2/common/SourceType.scala
+++ src/main/scala/org/specs2/common/SourceType.scala
@@ -5,7 +5,13 @@ package org.specs2.common
   */
 object SourceType {
 
+  /**
+    * Tags Scalaz specific code
+    */
   trait ScalazSpecific
+  /**
+    * Tags Cats specific code
+    */
   trait CatsSpecific
 
 }
