2550515f3f387b0e9fa86fbd8b564acf98d8ad12
SemanticBeeng <nick@email.com>
2016-06-06 19:31:51 +0000

    (todo) is there a way to write something like this compiles ...?

--- src/main/scala/org/specs2/typeclass/definition/package.scala
+++ src/main/scala/org/specs2/typeclass/definition/package.scala
@@ -1,5 +1,7 @@
 package org.specs2.typeclass
 
+import org.specs2.specification.{script, Snippets}
+
 /**
   *
   */
@@ -9,10 +11,9 @@ package object definition {
     * @see [[org.fp.resources.Scala]] [[org.fp.resources.Scalaz]]
     * @source https://hyp.is/6TGlfiwMEea_38vRFJt4xQ/archive.is/jnGcW
     */
-  object ScalaSpec extends org.specs2.mutable.Specification {
+  object ScalaSpec extends script.Spec with Snippets { def is = sequential ^ s2"""
 
-    "Boiler plate code to define the type class related stuff manually".p
-    eg {
+     Boiler plate code to define the type class related stuff manually ${snippet{
 
       trait CanTruthy[A] {
         self =>
@@ -44,46 +45,57 @@ package object definition {
             implicit def F: CanTruthy[A] = ev
           }
       }
+     }}
 
-      "Here’s how we can define typeclass instances for Int:".p
-      implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys({
-        case 0 => false
-        case _ => true
-      })
+    Here’s how we can define typeclass instances for Int:${snippet{
+//        implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys({
+//          case 0 => false
+//          case _ => true
+//        })
+//
+//        import ToCanIsTruthyOps._
+//
+//        10.truthy must_== true
 
-      import ToCanIsTruthyOps._
+    }}
 
-      10.truthy must_== true
+    Next is for List[A]:${snippet{
 
-      "Next is for List[A]:".p
-      implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.truthys({
-        case Nil => false
-        case _   => true
-      })
+//        implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.truthys({
+//          case Nil => false
+//          case _   => true
+//        })
+//
+//        List("foo").truthy must_== true
+    }}
 
-      List("foo").truthy must_== true
+    It looks like we need to treat Nil specially because of the nonvariance.${snippet{
 
-      "It looks like we need to treat Nil specially because of the nonvariance.".p
-      implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.truthys(_ => false)
-      Nil.truthy must_== false
+//      implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.truthys(_ => false)
+//      Nil.truthy must_== false
 
-      "And for Boolean using identity:".p
-      implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.truthys(identity)
+    }}
 
-      false.truthy must_== false
+    And for Boolean using identity: ${snippet{
+//      implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.truthys(identity)
+//
+//      false.truthy must_== false
+    }}
 
-      "Using CanTruthy typeclass, let’s define truthyIf like LYAHFGG:".p
-       //Now let’s make a function that mimics the if statement, but that works with YesNo values.
+    Using CanTruthy typeclass, let’s define truthyIf like LYAHFGG:
+        Now let’s make a function that mimics the if statement, but that works with YesNo values.
+    To delay the evaluation of the passed arguments, we can use pass-by-name: ${snippet{
 
-      "To delay the evaluation of the passed arguments, we can use pass-by-name:".p
-      def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
-        if (cond.truthy) ifyes
-        else ifno
+//      def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
+//        if (cond.truthy) ifyes
+//        else ifno
+//
+//      truthyIf (Nil:List[String]) {"YEAH!"} {"NO!"} must_== "NO!"
+//      truthyIf (2 :: 3 :: 4 :: Nil) {"YEAH!"} {"NO!"} must_== "YEAH!"
+//      truthyIf (true) {"YEAH!"} {"NO!"} must_== "YEAH!"
 
-      truthyIf (Nil:List[String]) {"YEAH!"} {"NO!"} must_== "NO!"
-      truthyIf (2 :: 3 :: 4 :: Nil) {"YEAH!"} {"NO!"} must_== "YEAH!"
-      truthyIf (true) {"YEAH!"} {"NO!"} must_== "YEAH!"
-    }
+    }}
+    """
   }
 
   /**
