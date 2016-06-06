f98aad7b27de574f4dbcd00505c3b1e251730549
SemanticBeeng <nick@email.com>
2016-06-06 22:12:20 +0000

    (checkpoint) typeclass definition for the standard Scala way works (rewritten) see todo

--- src/main/scala/org/specs2/typeclass/definition/package.scala
+++ src/main/scala/org/specs2/typeclass/definition/package.scala
@@ -11,42 +11,47 @@ package object definition {
     */
   object ScalaSpec extends org.specs2.mutable.Specification {
 
-    "Boiler plate code to define the type class related stuff manually".p
+    object defs {
+      "Boiler plate code to define the type class related stuff manually".p
 
-    trait CanTruthy[A] {
-      self =>
-      /** @return true, if `a` is truthy. */
-      def truthys(a: A): Boolean
-    }
+      trait CanTruthy[A] {
+        self =>
+        /** @return true, if `a` is truthy. */
+        def truthys(a: A): Boolean
+      }
 
-    object CanTruthy {
-      def apply[A](implicit ev: CanTruthy[A]): CanTruthy[A] = ev
+      object CanTruthy {
+        def apply[A](implicit ev: CanTruthy[A]): CanTruthy[A] = ev
 
-      def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
-        def truthys(a: A): Boolean = f(a)
+        def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
+          def truthys(a: A): Boolean = f(a)
+        }
+      }
+
+      trait CanTruthyOps[A] {
+        def self: A
+
+        implicit def F: CanTruthy[A]
+
+        final def truthy: Boolean = F.truthys(self)
+      }
+
+      object ToCanIsTruthyOps {
+        implicit def toCanIsTruthyOps[A](v: A)(implicit ev: CanTruthy[A]) =
+          new CanTruthyOps[A] {
+            def self = v
+
+            implicit def F: CanTruthy[A] = ev
+          }
       }
     }
 
-    trait CanTruthyOps[A] {
-      def self: A
-
-      implicit def F: CanTruthy[A]
-
-      final def truthy: Boolean = F.truthys(self)
-    }
-
-    object ToCanIsTruthyOps {
-      implicit def toCanIsTruthyOps[A](v: A)(implicit ev: CanTruthy[A]) =
-        new CanTruthyOps[A] {
-          def self = v
-
-          implicit def F: CanTruthy[A] = ev
-        }
-    }
+    //todo: how do I inline/print "defs" here?
 
     eg {
       "Here’s how we can define typeclass instances for Int:".p
 
+      import defs._
       implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys({
         case 0 => false
         case _ => true
@@ -60,6 +65,7 @@ package object definition {
     eg {
       "Next is for List[A]: ".p
 
+      import defs._
       implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.truthys({
         case Nil => false
         case _ => true
@@ -73,6 +79,7 @@ package object definition {
     eg {
       "It looks like we need to treat Nil specially because of the nonvariance.".p
 
+      import defs._
       implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.truthys(_ => false)
 
       import ToCanIsTruthyOps._
@@ -83,6 +90,7 @@ package object definition {
     eg {
       "And for Boolean using identity: ".p
 
+      import defs._
       implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.truthys(identity)
 
       import ToCanIsTruthyOps._
@@ -94,6 +102,8 @@ package object definition {
       "Using CanTruthy typeclass, let’s define truthyIf like LYAHFGG ".p
       "<i>Now let’s make a function that mimics the if statement, but that works with YesNo values.</i>".p
       "To delay the evaluation of the passed arguments, we can use pass-by-name: ".p
+
+      import defs._
 
       import ToCanIsTruthyOps._
       def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
