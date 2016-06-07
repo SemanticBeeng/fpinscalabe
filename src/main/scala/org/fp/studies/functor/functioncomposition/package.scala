2c5be4278eaba848e9de21f5b6daa79fed291781
SemanticBeeng <nick@email.com>
2016-06-07 17:08:29 +0000

    this works in SBT despite an error in ntellij

--- src/main/scala/org/fp/studies/functor/functioncomposition/package.scala
+++ src/main/scala/org/fp/studies/functor/functioncomposition/package.scala
@@ -31,17 +31,17 @@ package object functioncomposition {
     //@todo
 //    import org.specs2.matcher.{ShouldExpectations=>_,_}
 //    import org.specs2.matcher.{MustExpectations1=>_,_}
-    import org.specs2.matcher.{Expectable=>_,_}
+    //import org.specs2.matcher.{Expectable=>_,_}
     import cats.syntax.functor._
+    import cats.std.function._
 
     val inc = (x: Int) => x + 1
     val timesTwo = (x: Int) => x * 2
 
-    //@todo still matchs map to Expectable
-    //(inc map timesTwo)(3) must_== 8
+    (inc map timesTwo)(3) must_== 8
 
-    //val h = ((x: Int) => x + 1) map {_ * 7}
-    success
+//    val h = ((x: Int) => x + 1) map {_ * 7}
+//    h(1) must_== 15
   }
 
 }
