fef0f49276bb81650349171d0a846f17b28341d0
SemanticBeeng <nick@email.com>
2016-07-11 15:25:45 +0000

    (wip) introducing the apply functor refactored to move applicative functor examples down to the apply functor

--- src/main/scala/org/fp/studies/applicative/operators/custom/package.scala
+++ src/main/scala/org/fp/studies/applicative/operators/custom/package.scala
@@ -8,6 +8,7 @@ import scala.language.higherKinds
 
 //
 import org.specs2.specification.dsl.mutable.AutoExamples
+import org.fp.studies.apply.operators.custom.{AmountExample_ApplyScalaz, AmountExample_ApplyCats}
 
 //
 
@@ -16,161 +17,7 @@ import org.specs2.specification.dsl.mutable.AutoExamples
   */
 package object custom {
 
-  import org.fp.studies.functor.operators.withcustommap.AmountExample
-
-  /**
-    * @see [[Scalaz]]
-    */
-  object AmountExample_ApplicativeScalaz extends AmountExample {
-
-    import scalaz.Applicative
-
-    implicit val applicative: Applicative[Amount] = new Applicative[Amount] {
-
-      def point[A](a: => A): Amount[A] = One(a)
-
-      def ap[A, B](fa: => Amount[A])(f: => Amount[A => B]): Amount[B] =
-        fa match {
-          case One(a) =>
-            f match {
-              case One(g) => One(g(a))
-              case Couple(g, _) => One(g(a))
-              case Few(g, _, _) => One(g(a))
-            }
-          case Couple(a, b) =>
-            f match {
-              case One(g) => Couple(g(a), g(b))
-              case Couple(g, _) => Couple(g(a), g(b))
-              case Few(g, _, _) => Couple(g(a), g(b))
-            }
-          case Few(a, b, c) =>
-            f match {
-              case One(g) => Few(g(a), g(b), g(c))
-              case Couple(g, _) => Few(g(a), g(b), g(c))
-              case Few(g, h, i) => Few(g(a), h(b), i(c))
-            }
-        }
-    }
-  }
-
-  /**
-    * @see [[Cats]]
-    */
-  object AmountExample_ApplicativeCats extends AmountExample {
-
-    import cats.Applicative
-
-    implicit val applicative: Applicative[Amount] = new Applicative[Amount] {
-
-      def pure[A](a: A): Amount[A] = One(a)
-
-      def ap[A, B](f: Amount[A => B])(fa: Amount[A]): Amount[B] =
-
-        fa match {
-          case One(a) =>
-            f match {
-              case One(g) => One(g(a))
-              case Couple(g, _) => One(g(a))
-              case Few(g, _, _) => One(g(a))
-            }
-          case Couple(a, b) =>
-            f match {
-              case One(g) => Couple(g(a), g(b))
-              case Couple(g, _) => Couple(g(a), g(b))
-              case Few(g, _, _) => Couple(g(a), g(b))
-            }
-          case Few(a, b, c) =>
-            f match {
-              case One(g) => Few(g(a), g(b), g(c))
-              case Couple(g, _) => Few(g(a), g(b), g(c))
-              case Few(g, h, i) => Few(g(a), h(b), i(c))
-            }
-        }
-    }
-  }
-
   object Spec extends org.specs2.mutable.Spec with AutoExamples {
-
-    s"$keyPoint The $applicativeFunctor $operatorApply can have a few equivalent forms:"
-    s"$bookmarks $ann_ApplicativeExtractsFunction2"
-    eg {
-      /** in [[Scalaz]] */
-
-      import scalaz.syntax.applicative._
-      import scalaz.std.list._
-
-      ^(List(1, 2), List(3)) { _ * _ } must_== List(3, 6)
-      (List(1, 2) |@| List(3)) { _ * _ } must_== List(3, 6)
-
-      import scalaz.Apply
-      import scalaz.std.option._
-      import scalaz.syntax.std.option._
-      import scalaz.syntax.applicative._
-
-      val times = {(_: Int) * (_:Int)}
-
-      ^(2.some, Some(3))(times) must_== Some(6)
-      //@todo (Some(1) |@| Some(2)) (times) must_== Some(3)
-
-      import AmountExample_ApplicativeScalaz._
-
-      ^(One(6): Amount[Int], One(7)) { _ * _ } must_== One(42)
-      ((One(6): Amount[Int]) |@| One(7)) { _ * _ } must_== One(42)
-    }
-
-    eg { /** in [[Cats]] */
-      import cats.syntax.applicative._
-      import AmountExample_ApplicativeCats._
-
-      //@todo
-      success
-    }
-
-    s"$keyPoint The $applicativeFunctor $operatorApply applies the function from inside the second $functor:"
-    s"$bookmarks $ann_ApplicativeExtractsFunction, $ann_AppStyle2 "
-    eg {
-      /** in [[Scalaz]] */
-
-      import scalaz.syntax.applicative._
-      import AmountExample_ApplicativeScalaz._
-
-      (One(21): Amount[Int]) <*> One({ x: Int => x * 2 }) must_== One(42)
-
-      /**
-        * @doesnotcompile
-        *
-        * One({ x: Int => x * 2 }) <*> (One(21): Amount[Int]) must_== One(42)
-        */
-    }
-
-    eg { /** in [[Cats]] */
-      import cats.Applicative
-      import cats.syntax.applicative._
-      import AmountExample_ApplicativeCats._
-
-      //@todo
-      success
-    }
-
-    s"$keyPoint Applying $applicativeFunctor $operatorLHS and $operatorRHS to extract a projection:"
-    eg {
-      /** in [[Scalaz]] */
-
-      import scalaz.syntax.applicative._
-      import AmountExample_ApplicativeScalaz._
-
-      (One(6): Amount[Int]) <* One(7) must_== One(6)
-
-      (One(6): Amount[Int]) *> One(7) must_== One(7)
-    }
-
-    eg { /** in [[Cats]] */
-      import cats.syntax.applicative._
-      import AmountExample_ApplicativeCats._
-
-      //@todo
-      success
-    }
 
     /**
       * [[ann_applicativeLaws]]
@@ -191,7 +38,7 @@ package object custom {
 
       import scalaz.Applicative
       import scalaz.syntax.applicative._
-      import AmountExample_ApplicativeScalaz._
+      import AmountExample_ApplyScalaz._
 
       //Applicative[Amount].sequence(List(One(2), One(3), One(7))) must_== One(List(2,3,7))
       //List(One(2), One(3), One(7)).sequence must_== One(List(2,3,7))
@@ -205,7 +52,7 @@ package object custom {
     eg { /** in [[Cats]] */
       import cats.Applicative
       import cats.syntax.applicative._
-      import AmountExample_ApplicativeCats._
+      import AmountExample_ApplyCats._
 
       //@todo
       success
---
+++ src/main/scala/org/fp/studies/apply/operators/dfault/package.scala
@@ -0,0 +1,84 @@
+package org.fp.studies.apply.operators
+
+import org.fp.concepts._
+import org.fp.resources._
+import org.fp.bookmarks._
+
+import scala.language.higherKinds
+
+//
+import org.specs2.specification.dsl.mutable.{TextDsl, AutoExamples}
+
+/**
+  *
+  */
+package object dfault {
+
+  object Spec extends org.specs2.mutable.Spec with AutoExamples with TextDsl {
+
+    s"$keyPoint So far, when we were mapping functions over $functor-, we usually mapped functions that take only one parameter. " +
+      s"But what happens when we $operatorMap a function like *, which takes two parameters, over a $functor?"
+
+    s"$bookmarks $ann_ApplicativeMoreThanFunctor "
+
+    eg { /** in [[Scala]] */
+
+      val times = {(_: Int) * (_:Int)}
+      /**
+        * @doesnotcompile
+        *
+        *  List(1, 2, 3, 4) map times
+        */
+
+      s"We have to curry the function to $operatorMap and we get a list of partial functions:".p
+      val partialFuncList = List(1, 2, 3, 4) map times.curried
+
+      s"Now, we can map with the second parameter of 'times' to get the results.".p
+      partialFuncList.map(f => f(9)) must_== List(9, 18, 27, 36)
+    }
+
+    s"$keyPoint So $applicativeFunctor extends another $typeClass Apply, and introduces $operatorPoint and its alias 'pure'." +
+      s"LYAHFGG: " +
+      s"  $operatorPoint should take a value of any type and return an $applicativeValue with that value inside it. … " +
+      s"A better way of thinking about $operatorPoint would be to say that it takes a value and puts it in some sort " +
+      s"of default (or pure) context—a minimal context that still yields that value."
+
+    s"$keyPoint $Scalaz likes the name point instead of pure, and it seems like it’s basically a constructor that takes value A and returns F[A]. " +
+      s"It doesn't introduce an operator, but it introduces point method ($operatorPoint) and its symbolic alias η to all data types."
+
+    s"$bookmarks: $ann_ApplicativeAsTypeConstructor"
+
+    eg { /** in [[Scalaz]] */
+
+      import scalaz.std.list._
+      import scalaz.std.option._
+      import scalaz.syntax.applicative._
+
+      1.point[List] must_== List(1)
+      1.point[List] map {_ + 2} must_== List(3)
+      1.η[List] map {_ + 2} must_== List(3)
+
+      1.point[Option] must_== Some(1)
+      1.point[Option] map {_ + 2} must_== Some(3)
+      1.η[Option] map {_ + 2} must_== Some(3)
+    }
+
+    eg { /** in [[Cats]] */
+
+      import cats.std.list._
+      import cats.std.option._
+      import cats.syntax.applicative._
+
+      1.pure[List] must_== List(1)
+      1.pure[List] map {_ + 2} must_== List(3)
+      //@todo 1.η[List] map {_ + 2} must_== List(3)
+
+      1.pure[Option] must_== Some(1)
+      1.pure[Option] map {_ + 2} must_== Some(3)
+      //@todo 1.η[Option] map {_ + 2} must_== Some(3)
+    }
+
+    s"I can’t really express it in words yet, but there’s something cool about the fact that $constructor is abstracted out.".p
+
+  }
+}
