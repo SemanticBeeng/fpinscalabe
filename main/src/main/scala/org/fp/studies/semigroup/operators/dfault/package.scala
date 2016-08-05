d1cee1e86cb1c3ba897725a43439219e5d4b96d5
SemanticBeeng <nick@email.com>
2016-08-05 12:02:23 +0000

    (wip) introducing semigroup (applied to Maps)

--- main/src/main/scala/org/fp/studies/semigroup/operators/dfault/package.scala
+++ main/src/main/scala/org/fp/studies/semigroup/operators/dfault/package.scala
@@ -5,7 +5,6 @@ import org.fp.resources._
 import org.fp.bookmarks._
 
 import scala.language.higherKinds
-import scalaz.std
 
 //
 import org.specs2.specification.dsl.mutable.{TextDsl, AutoExamples}
@@ -55,6 +54,38 @@ package object dfault {
       Semigroup[Option[Int]].combine(1.some, 2.some)     must_== 3.some
       Semigroup[Option[Int]].combine(1.some, f_some(2))  must_== 3.some
       Semigroup[Option[Int]].combine(1.some, None)       must_== 1.some
+    }
+
+    s"$keyPoint Map forms a $semigroup"
+    s"$bookmarks ..."
+
+    eg { /** in [[Scalaz]] */
+
+//      import scalaz._
+      import scalaz.Scalaz._
+      import scalaz.std.AllInstances
+      import scalaz.Semigroup
+     import scalaz.Semigroup._
+//      import scalaz.std.map._
+      import scalaz.syntax.std.map._
+//      import scalaz.syntax.semigroup._
+//      import syntax.monoid._
+
+      val map1 = Map(1 -> 9 , 2 -> 20)
+      val map2 = Map(1 -> 100, 3 -> 300)
+
+      map1 |+| map2 must_== Map(1 -> 109, 2 -> 20, 3 -> 300)
+      map1.mappend(map2) must_== Map(1 -> 109, 2 -> 20, 3 -> 300)
+    }
+
+    eg {
+      /** in [[Cats]] */
+
+      import cats.Semigroup
+      import cats.std.option._
+      import cats.syntax.option._
+
+      success
     }
   }
 }
