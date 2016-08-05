e5b458e24c5284517c71f9c19f69ae5e3f0823ef
SemanticBeeng <nick@email.com>
2016-08-05 09:10:33 +0000

    (wip) introducing semigroup

--- main/src/main/scala/org/fp/studies/semigroup/operators/dfault/package.scala
+++ main/src/main/scala/org/fp/studies/semigroup/operators/dfault/package.scala
@@ -5,6 +5,7 @@ import org.fp.resources._
 import org.fp.bookmarks._
 
 import scala.language.higherKinds
+import scalaz.std
 
 //
 import org.specs2.specification.dsl.mutable.{TextDsl, AutoExamples}
@@ -16,18 +17,26 @@ package object dfault {
 
   object Spec extends org.specs2.mutable.Spec with AutoExamples with TextDsl {
 
-    s"$keyPoint ..."
-    s"$bookmarks .."
+    s"$keyPoint Option[Int] forms a $semigroup"
+    s"$bookmarks ..."
 
     eg { /** in [[Scalaz]] */
 
       s"Option as $semigroup".p
 
-      import scalaz.std.option._
-      import scalaz.syntax.semigroup._
+      import scalaz.Semigroup
       import scalaz.syntax.std.option._
+      import scalaz.std.option._
 
-      success
+      s"But needs to be defined in Scalaz".p
+      implicit object IntSemigroup extends Semigroup[Int] {
+        def append(f1: Int, f2: => Int): Int = f1 + f2
+      }
+
+      val f_some : Int => Option[Int] = { i => i.some }
+
+      Semigroup[Option[Int]].append(1.some, 2.some) must_== 3.some
+      Semigroup[Option[Int]].append(1.some, f_some(2))  must_== 3.some
     }
 
     eg {
