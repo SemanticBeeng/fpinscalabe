38365dbf197dea6f3f95e40f9b11d525acefd725
SemanticBeeng <nick@email.com>
2016-08-05 09:36:38 +0000

    (wip) introducing semigroup
    Scalaz 'append' is Cats 'combine'

--- main/src/main/scala/org/fp/studies/semigroup/operators/dfault/package.scala
+++ main/src/main/scala/org/fp/studies/semigroup/operators/dfault/package.scala
@@ -22,13 +22,11 @@ package object dfault {
 
     eg { /** in [[Scalaz]] */
 
-      s"Option as $semigroup".p
-
       import scalaz.Semigroup
-      import scalaz.syntax.std.option._
       import scalaz.std.option._
+      import scalaz.syntax.std.option._
 
-      s"But needs to be defined in Scalaz".p
+      s"But needs to be defined in $Scalaz".p
       implicit object IntSemigroup extends Semigroup[Int] {
         def append(f1: Int, f2: => Int): Int = f1 + f2
       }
@@ -41,8 +39,20 @@ package object dfault {
 
     eg {
       /** in [[Cats]] */
-      //@todo
-      success
+
+      import cats.Semigroup
+      import cats.std.option._
+      import cats.syntax.option._
+
+      s"But needs to be defined in $Cats as well".p
+      implicit object IntSemigroup extends Semigroup[Int] {
+        def combine(f1: Int, f2: Int): Int = f1 + f2
+      }
+
+      val f_some : Int => Option[Int] = { i => i.some }
+
+      Semigroup[Option[Int]].combine(1.some, 2.some) must_== 3.some
+      Semigroup[Option[Int]].combine(1.some, f_some(2))  must_== 3.some
     }
   }
 }
