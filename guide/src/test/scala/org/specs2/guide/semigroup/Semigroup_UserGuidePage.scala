7261c926c8d13461184e1c4f1e1a93af0972c7f7
SemanticBeeng <nick@email.com>
2016-08-08 13:14:05 +0000

    (wip) introducing semigroup (applied to Maps)

--- guide/src/test/scala/org/specs2/guide/semigroup/Semigroup_UserGuidePage.scala
+++ guide/src/test/scala/org/specs2/guide/semigroup/Semigroup_UserGuidePage.scala
@@ -1,6 +1,7 @@
 package org.specs2.guide.semigroup
 
 import org.fp._
+//
 import org.fp.studies.semigroup.operators._
 import org.specs2.ugbase.UserGuidePage
 
@@ -11,12 +12,14 @@ object Semigroup_UserGuidePage extends UserGuidePage {
 
   def is = "Semigroup functor example".title ^ s2"""
 
-## Examples for a 'custom' ${concepts.semigroup} with ${resources.Scalaz.id} and ${resources.Cats.id} in
+## Examples for a 'default' ${concepts.semigroup} with ${resources.Scalaz.id} and ${resources.Cats.id}
 
  * ${link(dfault.Spec1).hide}
  * ${link(dfault.Spec2).hide}
- * ${link(custom.Spec).hide}
 
+## Examples for a 'custom' ${concepts.semigroup} with ${resources.Scalaz.id} and ${resources.Cats.id}
+
+ * ${link(custom.Spec).hide}
 }
 
 """
