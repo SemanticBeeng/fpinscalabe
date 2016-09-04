37ca92dbe7244219f327a22610300568aadc111f
SemanticBeeng <nick@email.com>
2016-09-04 20:52:44 +0000

    testing HTML generation
    Interestingly, these spec render well
    How come not all of them?
    
    Could it be that mutable specs do not render as well?
    
    Now snippets are not evaluated but renders badly...

--- main/src/test/scala/org/fp/studies/typeclass/definition/manual/ScalaSpec.scala
+++ main/src/test/scala/org/fp/studies/typeclass/definition/manual/ScalaSpec.scala
@@ -14,15 +14,85 @@ import org.specs2.specification.Snippets
   * @see [[org.fp.resources.Scala]] [[org.fp.resources.Scalaz]]
   * @see [[ann_makeYourOwnTypeClasses]]
   */
-object ScalaSpec extends org.specs2.Specification with Snippets {
+object ScalaSpec extends org.specs2.mutable.Specification with Snippets {
 
-  def is = s"Defining $typeClass-es manually".title ^ s2"""
+  s2"""Defining $typeClass-es manually
 
 ### Boiler plate code to define the $typeClass related stuff manually
 
    ${incl[manual.Definitions]}
 
 ### And now usages to exemplify
-   ${incl[manual.Usages]}
-      """
+      """.p
+
+  // 8<---
+  import Definitions._
+  // 8<---
+
+  "Some examples of usages".p
+
+  "Here’s how we can define typeclass instances for Int:".p
+  eg {
+    implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.fromTruthy({
+      case 0 => false
+      case _ => true
+    })
+
+    import CanTruthy.ops._
+    10.truthy must_== false
+  }
+
+  "Next is for List[A]: ".p
+  eg {
+
+    implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.fromTruthy({
+      case Nil => false
+      case _ => true
+    })
+
+    import CanTruthy.ops._
+    List("foo").truthy must_== true
+  }
+
+  "It looks like we need to treat Nil specially because of the nonvariance.".p
+  eg {
+
+    implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.fromTruthy(_ => false)
+
+    import CanTruthy.ops._
+    Nil.truthy must_== false
+  }
+
+  "And for Boolean using identity: ".p
+  eg {
+
+    implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.fromTruthy(identity)
+
+    import CanTruthy.ops._
+    false.truthy must_== false
+  }
+
+  s2"""Using CanTruthy typeclass, let’s define truthyIf like LYAHFGG
+  <i>Now let’s make a function that mimics the if statement, but that works with YesNo values.</i>
+  To delay the evaluation of the passed arguments, we can use pass-by-name: """.p
+  eg {
+
+    import CanTruthy.ops._
+    def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
+      if (cond.truthy) ifyes
+      else ifno
+
+    // duplicate but good to see what needs to be in scope
+    implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.fromTruthy({
+      case Nil => false
+      case _ => true
+    })
+
+    // duplicate but good to see what needs to be in scope
+    implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.fromTruthy(identity)
+
+    truthyIf(Nil: List[String]) { "YEAH!" } { "NO!" } must_== "NO!"
+    truthyIf(2 :: 3 :: 4 :: Nil) { "YEAH!" } { "NO!" } must_== "YEAH!"
+    truthyIf(true) { "YEAH!" } { "NO!" } must_== "YEAH!"
+  }
 }
