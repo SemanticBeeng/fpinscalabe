68d15fd85a4d2f61d6f28564b1cbdbfecf844c07
SemanticBeeng <nick@email.com>
2016-09-08 07:38:59 +0000

    testing HTML generation
    Renders well.
    Shippets are not evaluated.

--- main/src/test/scala/org/fp/studies/typeclass/definition/manual/ScalaSpec.scala
+++ main/src/test/scala/org/fp/studies/typeclass/definition/manual/ScalaSpec.scala
@@ -3,6 +3,9 @@ package org.fp.studies.typeclass.definition.manual
 import org.fp.bookmarks._
 import org.fp.concepts._
 import org.fp.resources._
+import org.specs2.ScalaCheck
+import org.specs2.common.CheckedSpec
+import org.specs2.specification.dsl.mutable.TextDsl
 
 //
 import org.fp.studies.typeclass.definition.manual
@@ -14,25 +17,22 @@ import org.specs2.specification.Snippets
   * @see [[org.fp.resources.Scala]] [[org.fp.resources.Scalaz]]
   * @see [[ann_makeYourOwnTypeClasses]]
   */
-object ScalaSpec extends org.specs2.mutable.Specification with Snippets {
+object ScalaSpec extends org.specs2.Specification with Snippets with ScalaCheck with CheckedSpec {
 
-  s2"""Defining $typeClass-es manually
+  def is = s2"""Defining $typeClass-es manually
 
 ### Boiler plate code to define the $typeClass related stuff manually
 
    ${incl[manual.Definitions]}
 
 ### And now usages to exemplify
-      """.p
 
-  // 8<---
-  import Definitions._
-  // 8<---
+  Here’s how we can define $typeClassInstance-s for `Int`:
+  ${snippet{
+    // 8<--
+    import Definitions._
+    // 8<--
 
-  "Some examples of usages".p
-
-  "Here’s how we can define typeclass instances for Int:".p
-  snippet {
     implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.fromTruthy({
       case 0 => false
       case _ => true
@@ -40,10 +40,14 @@ object ScalaSpec extends org.specs2.mutable.Specification with Snippets {
 
     import CanTruthy.ops._
     10.truthy must_== false
-  }
 
-  "Next is for List[A]: ".p
-  snippet {
+  }.offsetIs(-4)}
+
+  Next is for `List[A]`:
+  ${snippet{
+    // 8<--
+    import Definitions._
+    // 8<--
 
     implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.fromTruthy({
       case Nil => false
@@ -52,47 +56,61 @@ object ScalaSpec extends org.specs2.mutable.Specification with Snippets {
 
     import CanTruthy.ops._
     List("foo").truthy must_== true
-  }
+  }.offsetIs(-4)}
 
-  "It looks like we need to treat Nil specially because of the nonvariance.".p
-  snippet {
+  It looks like we need to treat `Nil` specially because of the nonvariance.
+  ${snippet {
+    // 8<--
+    import Definitions._
+    // 8<--
 
     implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.fromTruthy(_ => false)
 
     import CanTruthy.ops._
-    Nil.truthy must_== false
-  }
+    check(Nil.truthy must_== 2)
 
-  "And for Boolean using identity: ".p
-  snippet {
+  }.offsetIs(-4)}
+
+  And for `Boolean` using identity:
+  ${snippet{
+    // 8<--
+    import Definitions._
+    // 8<--
 
     implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.fromTruthy(identity)
 
     import CanTruthy.ops._
     false.truthy must_== false
-  }
 
-  s2"""Using CanTruthy typeclass, let’s define truthyIf like LYAHFGG
+  }.offsetIs(-4)}
+
+  Using `CanTruthy` $typeClass, let’s define `truthyIf` like LYAHFGG
   <i>Now let’s make a function that mimics the if statement, but that works with YesNo values.</i>
-  To delay the evaluation of the passed arguments, we can use pass-by-name: """.p
-  snippet {
+  To delay the evaluation of the passed arguments, we can use pass-by-name:
+  ${snippet{
+    // 8<--
+    import Definitions._
+    // 8<--
 
     import CanTruthy.ops._
     def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
       if (cond.truthy) ifyes
       else ifno
 
-    // duplicate but good to see what needs to be in scope
+    //@todo "Duplicate but good to see what needs to be in scope"
+
     implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.fromTruthy({
       case Nil => false
       case _ => true
     })
 
-    // duplicate but good to see what needs to be in scope
+    //@todo Duplicate but good to see what needs to be in scope
     implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.fromTruthy(identity)
 
     truthyIf(Nil: List[String]) { "YEAH!" } { "NO!" } must_== "NO!"
     truthyIf(2 :: 3 :: 4 :: Nil) { "YEAH!" } { "NO!" } must_== "YEAH!"
     truthyIf(true) { "YEAH!" } { "NO!" } must_== "YEAH!"
-  }
+
+  }.offsetIs(-4)}
+  """.stripMargin
 }
