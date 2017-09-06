7739d7d9b94a2869c61c854f221e4a9de601d7ae
SemanticBeeng <nick@email.com>
2017-09-06 05:10:41 -0700

    build cleanup

--- guide/src/test/scala/org/specs2/thirdparty/blogs/cakesolutions/Existential_types_in_Scala.scala
+++ guide/src/test/scala/org/specs2/thirdparty/blogs/cakesolutions/Existential_types_in_Scala.scala
@@ -4,8 +4,12 @@ import org.fp.concepts._
 import org.fp.bookmarks._
 
 import org.specs2.ugbase.UserGuidePage
+import org.fp.thirdparty.cakesolutions.Existential_types_in_Scala.snippets.Snip01
 
+import org.specs2.common.SnippetHelper._
+import org.specs2.execute.SnippetParams
 
+import scala.language.existentials
 
 /**
   * For [[org.fp.resources.cakeSolutions_ExistentialTypesInScala]]
@@ -15,6 +19,44 @@ object Existential_types_in_Scala extends UserGuidePage {
 def is = s"Existential types in Scala".title ^ s2"""
 
   ${ann_cakeSolutions_ExistentialTypesInScala_bkm1.md}
-  todo demo $existentialType
+
+### Existential Types
+
+Existential types ($existentialType) are like the normal $typeVariable-s we saw above, except the variable only shows up on the right:
+
+${incl[Snip01.type]}
+
+Now `A` only appears on the right side, which means that the final type, `F`, will not change regardless of what `A` is.
+For example:
+
+${snippet{
+  import Snip01._
+
+  val v1: FE = SomeClass("hello")
+  val v2: FE = SomeClass(1: Int)
+
+  case class User(name: String)
+  val user = User("joe")
+  val v3: FE = SomeClass(user)
+}}
+
+Side note: if A were to only appear on the left side, then A would be a $phantomType.
+We aren't covering those in this article though, so let's ignore them.
+
+Like before, let's now expand it. Beware, this expansion isn't as clean as the previous one:
+
+${snippet{
+  sealed trait Existential {
+    type Inner
+    val value: Inner
+  }
+
+  final case class MkEx[A](value: A) extends Existential { type Inner = A }
+}}
+
+Here we're using $pathDependentType in order to create a better interface for our `Existential` trait.
+We could just have an empty trait but that would mean that we would need to case match `MkEx` whenever we wanted to access
+its fields. However, the concept still holds and shares the properties we described above:
+
   """
 }
---
+++ main/src/test/scala/org/fp/thirdparty/cakesolutions/Existential_types_in_Scala/snippets/Snip01.scala
@@ -0,0 +1,11 @@
+package org.fp.thirdparty.cakesolutions.Existential_types_in_Scala.snippets
+
+object Snip01 {
+  //8<--
+  case class SomeClass[T](value: T)
+
+  type F[A] = SomeClass[A] // `A` appears on the left and right side, common case
+
+  type FE = SomeClass[A] forSome { type A } // `A` appears only on the right, existential case
+  //8<--
+}
