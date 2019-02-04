ed32d31cc573b79d8fd8366f26f6a2693c7260a8
SemanticBeeng <nick@email.com>
2019-02-03 23:42:01 -0800

    `applicative` specs

--- main/src/test/scala/org/fp/studies/applicative/operators/dfault/package.scala
+++ main/src/test/scala/org/fp/studies/applicative/operators/dfault/package.scala
@@ -16,7 +16,7 @@ package object dfault {
 
   object Spec extends org.specs2.mutable.Spec with AutoExamples with TextDsl {
 
-    s"$keyPoint ..."
+    s"$keyPoint $applyFunctor-s $operator_compose"
 
     s"$bookmarks: ..."
 
@@ -37,6 +37,34 @@ package object dfault {
       import cats.syntax.applicative._
 
       //@todo
+      success
+    }
+
+    eg { /** in [[Cats]] */
+      /**
+        * #hypis https://hyp.is/Beb4XihFEemvIjvPB2-mig/typelevel.org/cats/typeclasses/applicative.html
+        */
+
+      import cats.data.Nested
+      import cats.Applicative
+      import cats.implicits._
+      import scala.concurrent.Future
+      import scala.reflect.ClassTag
+
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      val x: Future[Option[Int]] = Future.successful(Some(5))
+      val y: Future[Option[Char]] = Future.successful(Some('a'))
+
+      val composed = Applicative[Future].compose[Option].map2(x, y)(_ + _)
+      // composed: scala.concurrent.Future[Option[Int]] = Future(<not completed>)
+
+      composed must beAnInstanceOf(implicitly[ClassTag[Future[Option[Int]]]])
+
+      val nested = Applicative[Nested[Future, Option, ?]].map2(Nested(x), Nested(y))(_ + _)
+      // nested: cats.data.Nested[scala.concurrent.Future,Option,Int] = Nested(Future(<not completed>))
+      nested must beAnInstanceOf(implicitly[ClassTag[Nested[Future, Option, Int]]]) //#todo use '?'
+
       success
     }
   }
